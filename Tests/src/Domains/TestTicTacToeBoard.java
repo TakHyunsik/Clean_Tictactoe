aaaf170b766b8fdfc59c106613d396239da2ccc6
taktakgyun <vbmrkk@naver.com>
2023-11-18 22:11:33 +0900

    ✅ test : Add to test check_result
    check_result 테스트 케이스 작성

--- Tests/src/Domains/TestTicTacToeBoard.java
+++ Tests/src/Domains/TestTicTacToeBoard.java
@@ -26,7 +26,7 @@ class TestTicTacToeBoard {
 	void test_set_and_get() {
 		assertTrue(board.set_in_board(1, 1, PlayerType.P1));
 		assertEquals(PlayerType.P1, board.get_in_board(1, 1));
-		System.out.println()
+
 		assertEquals(PlayerType.None, board.get_in_board(0, 0));
 		assertEquals(PlayerType.None, board.get_in_board(1, 0));
 		assertEquals(PlayerType.None, board.get_in_board(1, 2));
@@ -35,7 +35,7 @@ class TestTicTacToeBoard {
 
 	@Test
 	void test_check_setable_in_board() {
-		// ºó º¸µåÀÏ¶§ µÑ¼ö ÀÖ´ÂÁö È®ÀÎÇÑ´Ù.
+		// ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Ï¶ï¿½ ï¿½Ñ¼ï¿½ ï¿½Ö´ï¿½ï¿½ï¿½ È®ï¿½ï¿½ï¿½Ñ´ï¿½.
 		for (int i = 0; i < board.get_size(); i++) {
 			for (int j = 0; j < board.get_size(); j++) {
 				assertEquals(PlayerType.None, board.get_in_board(i, j));
@@ -43,21 +43,21 @@ class TestTicTacToeBoard {
 			}
 		}
 
-		// È®ÀÎÇÏ°í 1,1¿¡ µÐ´Ù.
+		// È®ï¿½ï¿½ï¿½Ï°ï¿½ 1,1ï¿½ï¿½ ï¿½Ð´ï¿½.
 		assertTrue(board.set_in_board(1, 1, PlayerType.P1));
 
-		// ÀÌ¹Ì µÐ°÷Àº µÑ¼ö ¾ø´Ù.
+		// ï¿½Ì¹ï¿½ ï¿½Ð°ï¿½ï¿½ï¿½ ï¿½Ñ¼ï¿½ ï¿½ï¿½ï¿½ï¿½.
 		assertTrue(!board.check_setable_in_board(1, 1));
 		assertTrue(!board.set_in_board(1, 1, PlayerType.P1));
 
-		// ¹üÀ§°¡ ³ÑÀ¸¸é µÑ¼ö ¾ø´Ù.
-		// - À½¿¡ ¹üÀ§ Å×½ºÆ®
+		// ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ñ¼ï¿½ ï¿½ï¿½ï¿½ï¿½.
+		// - ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½×½ï¿½Æ®
 		for (int i = -10; i < 0; i++) {
 			for (int j = -10; j < 0; j++) {
 				assertTrue(!board.check_setable_in_board(i, j));
 			}
 		}
-		// - ¾ç¿¡ ¹üÀ§ Å×½ºÆ®
+		// - ï¿½ç¿¡ ï¿½ï¿½ï¿½ï¿½ ï¿½×½ï¿½Æ®
 		for (int i = Math.min(board.get_size(), 50); i < 50; i++) {
 			for (int j = Math.min(board.get_size(), 50); j < 50; j++) {
 				assertTrue(!board.check_setable_in_board(i, j));
@@ -65,11 +65,480 @@ class TestTicTacToeBoard {
 		}
 	}
 
-}
+	@Test
+	void test_full_empty() {
+		// ì‹œìž‘ì€ ë¹„ì–´ìžˆë‹¤.
+		assertTrue(board.check_empty());
+		assertFalse(board.check_be_full());
 
-// assertArrayEquals(a,b) : ?ò÷ a??b?? ??????? ???
-// assertEquals(a,b) : ??? a??b?? ???? ?????? ???
-// assertSame(a,b) : ??? a??b?? ???? ??????? ???
-// assertTrue(a) : a?? ?????? ???
-// assertNotNull(a) : a????? null?? ????? ???
-// fail("Not yet implemented");
+		for (int i = 0; i < board.get_size() * board.get_size() - 1; i++) {
+			int y = i / board.get_size(), x = (i % board.get_size());
+			if (i % 2 == 0) {
+				assertTrue(board.set_in_board(y, x, PlayerType.P1));
+			} else {
+				assertTrue(board.set_in_board(y, x, PlayerType.P2));
+			}
+			assertFalse(board.check_empty());
+			assertFalse(board.check_be_full());
+		}
+		assertTrue(board.set_in_board(2, 2, PlayerType.P1));
+		assertFalse(board.check_empty());
+		assertTrue(board.check_be_full());
+	}
+
+	@Test
+	void test_sze() {
+		assertEquals(3, board.get_size());
+	}
+
+	@Test
+	public void testcheck_resultNoWinner() {
+		// -------
+		// |O|X|O|
+		// -------
+		// |X|O|X|
+		// -------
+		// |X|O|X|
+		// -------
+
+		// No winner, the result should be PlayerType.None
+		assertEquals(PlayerType.None, board.check_result());
+
+		// ë¬´ìŠ¹ë¶€
+		board.set_in_board(0, 0, PlayerType.P1);
+		board.set_in_board(0, 1, PlayerType.P2);
+		board.set_in_board(0, 2, PlayerType.P1);
+		board.set_in_board(1, 0, PlayerType.P2);
+		board.set_in_board(1, 1, PlayerType.P1);
+		board.set_in_board(1, 2, PlayerType.P2);
+		board.set_in_board(2, 0, PlayerType.P2);
+		board.set_in_board(2, 1, PlayerType.P1);
+		board.set_in_board(2, 2, PlayerType.P2);
+		// The game is a tie, the result should be PlayerType.None
+		assertEquals(PlayerType.None, board.check_result());
+		assertTrue(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner1() {
+		// -------
+		// |O|O|O|
+		// -------
+		// |X|X| |
+		// -------
+		// | |X| |
+		// -------
+		board.set_in_board(0, 0, PlayerType.P1);
+		board.set_in_board(0, 1, PlayerType.P1);
+		board.set_in_board(0, 2, PlayerType.P1);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner2() {
+		// -------
+		// |X| |X|
+		// -------
+		// |O|O|O|
+		// -------
+		// |X|X| |
+		// -------
+		board.set_in_board(1, 0, PlayerType.P1);
+		board.set_in_board(1, 1, PlayerType.P1);
+		board.set_in_board(1, 2, PlayerType.P1);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner3() {
+		// -------
+		// | |X|X|
+		// -------
+		// |X|X| |
+		// -------
+		// |O|O|O|
+		// -------
+		board.set_in_board(2, 0, PlayerType.P1);
+		board.set_in_board(2, 1, PlayerType.P1);
+		board.set_in_board(2, 2, PlayerType.P1);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner1_2() {
+		// -------
+		// |X|X|X|
+		// -------
+		// |O|O| |
+		// -------
+		// | | |O|
+		// -------
+		board.set_in_board(0, 0, PlayerType.P2);
+		board.set_in_board(0, 1, PlayerType.P2);
+		board.set_in_board(0, 2, PlayerType.P2);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner2_2() {
+		// -------
+		// |O| |O|
+		// -------
+		// |X|X|X|
+		// -------
+		// |O|O| |
+		// -------
+		board.set_in_board(1, 0, PlayerType.P2);
+		board.set_in_board(1, 1, PlayerType.P2);
+		board.set_in_board(1, 2, PlayerType.P2);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+	}
+
+	@Test
+	public void testcheck_resultHorizontalWinner3_3() {
+		// -------
+		// |O|X|O|
+		// -------
+		// |O|O|X|
+		// -------
+		// |X|X|X|
+		// -------
+		board.set_in_board(2, 0, PlayerType.P2);
+		board.set_in_board(2, 1, PlayerType.P2);
+		board.set_in_board(2, 2, PlayerType.P2);
+		// Player 1 wins horizontally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertTrue(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner1() {
+		// -------
+		// |X|O|O|
+		// -------
+		// |X|O|X|
+		// -------
+		// |X|X|O|
+		// -------
+		board.set_in_board(0, 0, PlayerType.P2);
+		board.set_in_board(1, 0, PlayerType.P2);
+		board.set_in_board(2, 0, PlayerType.P2);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertTrue(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner2() {
+		// -------
+		// |O|X|O|
+		// -------
+		// |X|X| |
+		// -------
+		// |O|X|O|
+		// -------
+		board.set_in_board(0, 1, PlayerType.P2);
+		board.set_in_board(1, 1, PlayerType.P2);
+		board.set_in_board(2, 1, PlayerType.P2);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner3() {
+		// -------
+		// |O|O|X|
+		// -------
+		// |O| |X|
+		// -------
+		// |X|O|X|
+		// -------
+		board.set_in_board(0, 2, PlayerType.P2);
+		board.set_in_board(1, 2, PlayerType.P2);
+		board.set_in_board(2, 2, PlayerType.P2);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner1_1() {
+		// -------
+		// |O|X| |
+		// -------
+		// |O|X|X|
+		// -------
+		// |O| |X|
+		// -------
+		board.set_in_board(0, 0, PlayerType.P1);
+		board.set_in_board(1, 0, PlayerType.P1);
+		board.set_in_board(2, 0, PlayerType.P1);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner2_1() {
+		// -------
+		// |X|O|X|
+		// -------
+		// |O|O|X|
+		// -------
+		// |X|O| |
+		// -------
+		board.set_in_board(0, 1, PlayerType.P1);
+		board.set_in_board(1, 1, PlayerType.P1);
+		board.set_in_board(2, 1, PlayerType.P1);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P1, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultVerticalWinner3_1() {
+		// -------
+		// |X|X|O|
+		// -------
+		// |O| |O|
+		// -------
+		// |X|X|O|
+		// -------
+		assertTrue(board.check_empty());
+		board.set_in_board(0, 2, PlayerType.P1);
+		board.set_in_board(1, 2, PlayerType.P1);
+		board.set_in_board(2, 2, PlayerType.P1);
+		// Player 2 wins vertically, the result should be PlayerType.P2
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P1, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultDiagonalWinner1() {
+		// -------
+		// |O| |X|
+		// -------
+		// |X|O|X|
+		// -------
+		// |X| |O|
+		// -------
+		board.set_in_board(0, 0, PlayerType.P1);
+		board.set_in_board(1, 1, PlayerType.P1);
+		board.set_in_board(2, 2, PlayerType.P1);
+		// Player 1 wins diagonally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultDiagonalWinner2() {
+		// -------
+		// |X|O| |
+		// -------
+		// |O|X|O|
+		// -------
+		// | |O|X|
+		// -------
+		board.set_in_board(0, 0, PlayerType.P2);
+		board.set_in_board(1, 1, PlayerType.P2);
+		board.set_in_board(2, 2, PlayerType.P2);
+		// Player 1 wins diagonally, the result should be PlayerType.P1
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultAntiDiagonalWinner1() {
+		// -------
+		// | |O|X|
+		// -------
+		// |O|X| |
+		// -------
+		// |X|O|O|
+		// -------
+		board.set_in_board(0, 2, PlayerType.P2);
+		board.set_in_board(1, 1, PlayerType.P2);
+		board.set_in_board(2, 0, PlayerType.P2);
+		// Player 2 wins anti-diagonally, the result should be PlayerType.P2
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P1);
+		assertEquals(PlayerType.P2, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P2, board.check_result());
+		assertFalse(board.check_empty());
+		assertFalse(board.check_be_full());
+	}
+
+	@Test
+	public void testcheck_resultAntiDiagonalWinner2() {
+		// -------
+		// |X|X|O|
+		// -------
+		// |X|O|O|
+		// -------
+		// |O|O|X|
+		// -------
+		board.set_in_board(0, 2, PlayerType.P1);
+		board.set_in_board(1, 1, PlayerType.P1);
+		board.set_in_board(2, 0, PlayerType.P1);
+		// Player 2 wins anti-diagonally, the result should be PlayerType.P2
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 2, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(0, 1, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 0, PlayerType.P2);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(2, 1, PlayerType.P1);
+		assertEquals(PlayerType.P1, board.check_result());
+		board.set_in_board(1, 2, PlayerType.P1);
+		assertEquals(PlayerType.P1, board.check_result());
+		assertFalse(board.check_empty());
+		assertTrue(board.check_be_full());
+	}
+}
