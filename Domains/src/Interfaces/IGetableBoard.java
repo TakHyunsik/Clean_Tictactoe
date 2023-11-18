5610c7f61e3bb150e58a3ed0859fd901fdfac973
taktakgyun <vbmrkk@naver.com>
2023-11-18 22:24:42 +0900

    🎨 chore : Change code

--- Domains/src/Interfaces/IGetableBoard.java
+++ Domains/src/Interfaces/IGetableBoard.java
@@ -3,5 +3,5 @@ package Interfaces;
 import Commons.PlayerType;
 
 public interface IGetableBoard {
-	public Commons.PlayerType get_in_board(int y, int x);
+	public PlayerType get_in_board(int y, int x);
 }
